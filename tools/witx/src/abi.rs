3bf0c0c2ebb0b249386daaca354cf352f8ecbd99
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-05-22 09:22:36 +0200

    Fixed enums which where not correctly using i32

--- tools/witx/src/abi.rs
+++ tools/witx/src/abi.rs
@@ -455,6 +455,9 @@ impl InterfaceFunc {
         let mut results = Vec::new();
         for param in self.params.iter() {
             match &**param.tref.type_() {
+                Type::Varient(v) if v.is_enum()
+                    => params.push(WasmType::I32),
+
                 Type::Pointer(_)
                 | Type::ConstPointer(_)
                 | Type::Variant(_) => params.push(WasmType::I64),
